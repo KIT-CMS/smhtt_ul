f78c11bd62c746ba129c385e3231f9a699937f2e
nshadskiy <nikita.shadskiy@kit.edu>
2023-08-01 10:48:17 +0200

    adjusting setup files

--- utils/setup_root.sh
+++ utils/setup_root.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ $(hostname) =~ portal1 || $(hostname) =~ centos7 ]]; then
+if [[ $(hostname) =~ centos7 ]]; then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
-elif [[ $(hostname) =~ bms ]]; then
+elif [[ $(hostname) =~ bms || $(hostname) =~ portal1 ]]; then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos8-gcc11-opt/setup.sh
 fi
 
