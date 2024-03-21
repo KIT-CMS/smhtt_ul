4862953efa5f9a64b3ece1ceea03642a39572170
Olha Lavoryk <olhalavoryk@gmail.com>
2024-03-21 16:33:35 +0100

    Changes SSOS extrapolation factor to 1

--- shapes/do_estimations.py
+++ shapes/do_estimations.py
@@ -289,7 +289,7 @@ def main(args):
         for channel in qcd_inputs:
             for category in qcd_inputs[channel]:
                 logger.info("Do estimation for category %s", category)
-                extrapolation_factor = 1.25
+                extrapolation_factor = 1.0
                 if channel in ["et", "mt"] and args.era == "2016":
                     extrapolation_factor = 1.17
                 elif channel in ["em"]:
