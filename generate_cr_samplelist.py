9ce6f4bc0d7343c2708feb4ac1a4891e4cdc9e64
Artur Monsch <60860160+a-monsch@users.noreply.github.com>
2024-03-26 09:29:44 +0100

    Adding small script to retrieve used samples for ntuple production + minor changes

---
+++ generate_cr_samplelist.py
@@ -0,0 +1,24 @@
+import argparse
+from config.shapes.file_names import files
+
+parser = argparse.ArgumentParser(description='Create a list of samples for SM HTT UL analysis')
+parser.add_argument('--channel', nargs='+', type=str, help='Channel(s): mm, mt, et, em, tt')
+parser.add_argument('--era', nargs='+', type=str, help='Era(s): 2018, 2017, 2016preVFP, 2016postVFP')
+parser.add_argument('--output', type=str, default='samples.txt', help='Output file name')
+option = parser.parse_args()
+
+print(f"Selected years: {option.era}")
+print(f"Selected channels: {option.channel}")
+
+if __name__ == "__main__":
+    used_files = []
+    for era in option.era:
+        for ch in option.channel:
+            for processes in files[era][ch]:
+                used_files += files[era][ch][processes]
+    used_files = sorted(list(set(used_files)))
+
+    with open(option.output, 'w') as f:
+        f.write("\n".join(used_files))
+
+    print(f"{len(used_files)} samples is saved in {option.output}")
