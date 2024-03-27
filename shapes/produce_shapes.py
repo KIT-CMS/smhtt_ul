12ea50f9e781c836edd13914760ca79cf3fa31b6
Olha Lavoryk <olhalavoryk@gmail.com>
2024-03-27 17:09:32 +0100

    Added emb mt trigger efficiency variation for mm channel

--- shapes/produce_shapes.py
+++ shapes/produce_shapes.py
@@ -1217,6 +1217,13 @@ def main(args):
                 variations=[same_sign],
                 enable_check=do_check,
             )
+            book_histograms(
+                um,
+                processes=embS,
+                datasets=nominals[era]["units"][channel],
+                variations=[trigger_eff_mt_emb],
+                enable_check=do_check,
+            )
         elif channel == "ee":
             book_histograms(
                 um,
