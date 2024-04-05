7e7ddf4ed8920e4c61b6f81350b91a8d11dc4551
Olha Lavoryk <olhalavoryk@gmail.com>
2024-04-05 13:57:21 +0200

    The last CMSSW version

---
+++ utils/setup_cmssw_tauid.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export SCRAM_ARCH=slc7_amd64_gcc1000
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
+
+# pushd CMSSW_10_2_21/src
+pushd CMSSW_12_6_5/src
+eval `scramv1 runtime -sh`
+popd
