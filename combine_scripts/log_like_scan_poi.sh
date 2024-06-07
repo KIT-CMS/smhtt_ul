850767a496483d2b502633c7e055acd355d56184
Olha Lavoryk <olhalavoryk@gmail.com>
2024-06-07 10:29:49 +0200

    Attached file with likelihood profile

--- combine_scripts/log_like_scan_poi.sh
+++ combine_scripts/log_like_scan_poi.sh
@@ -1,49 +1,47 @@
 source utils/setup_cmssw_tauid.sh
 source utils/bashFunctionCollection.sh
 
-TAG=$1
-NTUPLETAG=$2
-ERA=$3
-CHANNEL=$4
+
+CHANNEL=$1
+ERA=$2
+NTUPLETAG=$3
+TAG=$4
 WP=$5
-WP_ELE=$6
-
-
 
 categories=("DM0" "DM1" "DM10_11")
 
-
-tag_folder="medium_vs_ele_es_4_0"
 
 
 fix_es=0
 fix_id=0
 
-datacard_output_dm="datacards_es_4_0_29Apr_morph_v1/${NTUPLETAG}-${TAG}/${ERA}_tauid_${WP}"
+datacard_output_dm="datacards_dm_sim_fit_${TAG}/${NTUPLETAG}-${TAG}/${ERA}_tauid_${WP}"
 
+mH=127
 
+. combine_scripts/fit_id_es.sh 
 for dm_cat in ${categories[@]}; do
 
-    if [[ ! -d lscan_/${tag_folder}/${ERA}/${CHANNEL}/${TAG}/${dm_cat}/ ]]
+    if [[ ! -d lscan_/${TAG}/${ERA}/${CHANNEL}/${TAG}/${dm_cat}/ ]]
     then
-        mkdir -p lscan/${tag_folder}/${ERA}/${CHANNEL}/${WP}/${dm_cat}/
-        mkdir -p lscan/${tag_folder}/${ERA}/${CHANNEL}/${WP}/${dm_cat}/
+        mkdir -p lscan/${TAG}/${ERA}/${CHANNEL}/${WP}/${dm_cat}/
+        mkdir -p lscan/${TAG}/${ERA}/${CHANNEL}/${WP}/${dm_cat}/
     fi
 
     
     if [[ $dm_cat == "DM0" ]]; then
-        fix_es=-0.999
-        fix_id=0.994
+        fix_es=${fix_es_dm0}
+        fix_id=${fix_id_dm0}
     fi
 
     if [[ $dm_cat == "DM1" ]]; then
-        fix_es=-1.399 
-        fix_id=1.089
+        fix_es=${fix_es_dm1}
+        fix_id=${fix_id_dm1}
     fi
 
     if [[ $dm_cat == "DM10_11" ]]; then
-        fix_es=-1.193
-        fix_id=1.060
+        fix_es=${fix_es_dm10_11}
+        fix_id=${fix_id_dm10_11}
     fi
 
                                                    
@@ -55,15 +53,15 @@ for dm_cat in ${categories[@]}; do
     --robustFit=1 --setRobustFitAlgo=Minuit2  --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP \
     --cminFallbackAlgo Minuit2,Migrad,0:0.001 --cminFallbackAlgo Minuit2,Migrad,0:0.01 --cminPreScan \
     --redefineSignalPOIs ES_${dm_cat},r \
-    --floatOtherPOIs=1 --points=400 --algo grid -v 2 -m 127
+    --floatOtherPOIs=1 --points=400 --algo grid -v 2 -m ${mH}
     
     
-    python3  plot1DScan.py  higgsCombine.${fit_name}.MultiDimFit.mH127.root --POI r \
+    python3  tau_id_es_measurement/plot1DScan_comb.py  higgsCombine.${fit_name}.MultiDimFit.mH${mH}.root --POI r \
     --y-max 12 --output ${TAG}_${ERA}_${CHANNEL}_${WP}_${cat}_r_${dm_cat}
-    python3  plot1DScan.py  higgsCombine.${fit_name}.MultiDimFit.mH127.root --POI ES_${dm_cat} \
+    python3  tau_id_es_measurement/plot1DScan_comb.py  higgsCombine.${fit_name}.MultiDimFit.mH${mH}.root --POI ES_${dm_cat} \
      --y-max 12 --output ${TAG}_${ERA}_${CHANNEL}_${WP}_${cat}_es_${dm_cat}
-    mv ${TAG}_${ERA}_${CHANNEL}_${WP}_${cat}*  /work/olavoryk/tau_pog_tau_sfs/tau_id_es_main/smhtt_ul/lscan/${tag_folder}/${ERA}/${CHANNEL}/${WP}/${dm_cat}/
-
+    mv ${TAG}_${ERA}_${CHANNEL}_${WP}_${cat}*  lscan/${TAG}/${ERA}/${CHANNEL}/${WP}/${dm_cat}/
+    mv higgsCombine.${fit_name}.MultiDimFit.mH${mH}.root lscan/${TAG}/${ERA}/${CHANNEL}/${WP}/${dm_cat}/
 done
 # exit 0
 
