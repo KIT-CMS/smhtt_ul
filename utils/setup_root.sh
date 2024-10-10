efd47bce59453a511f4ffe5730e7761f7e0d4a54
Moritz Molch <moritz.molch@cern.ch>
2024-10-10 13:56:47 +0200

    Update ROOT setup for EL9 machines

--- utils/setup_root.sh
+++ utils/setup_root.sh
@@ -3,7 +3,7 @@
 if [[ $(hostname) =~ centos7 ]]; then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
 elif [[ $(hostname) =~ bms || $(hostname) =~ portal1 ]]; then
-    source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos8-gcc11-opt/setup.sh
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos9-gcc11-opt/setup.sh
 fi
 
 export PYTHONPATH=$PWD:$PYTHONPATH
