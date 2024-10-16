f2dd8f4af3bc45fb4a45c04c3cc3f7b97748a57e
Moritz Molch <moritz.molch@cern.ch>
2024-10-16 07:59:29 +0200

    Switch to newer LCG stack

--- utils/setup_root.sh
+++ utils/setup_root.sh
@@ -4,7 +4,7 @@ if [[ $(hostname) =~ centos7 ]]; then
     source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
     # source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc11-opt/setup.sh
 elif [[ $(hostname) =~ bms || $(hostname) =~ portal1 ]]; then
-    source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos9-gcc11-opt/setup.sh
+    source /cvmfs/sft.cern.ch/lcg/views/LCG_106/x86_64-el9-gcc13-opt/setup.sh
 fi
 
 export PYTHONPATH=$PWD:$PYTHONPATH
