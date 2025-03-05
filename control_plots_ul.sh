c3fdf347e2570c445236f993f4ae813c1ac09827
Simon Daigler <simon.daigler@student.kit.edu>
2025-03-05 16:36:08 +0100

    feat: adapt for non_res_hh

--- control_plots_ul.sh
+++ control_plots_ul.sh
@@ -30,7 +30,7 @@ echo "##########################################################################
 
     echo "running xsec friends script"
     echo "XSEC_FRIENDS: ${XSEC_FRIENDS}"
-    python3 friends/build_friend_tree.py --basepath $KINGMAKER_BASEDIR_XROOTD --outputpath root://cmsdcache-kit-disk.gridka.de/$XSEC_FRIENDS --nthreads 20
+    python3 friends/build_friend_tree.py --basepath $KINGMAKER_BASEDIR_XROOTD --outputpath $XSEC_FRIENDS --nthreads 20
 fi
 
 if [[ $MODE == "SHAPES" ]]; then
