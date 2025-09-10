dcd81a3687fe9e60c49b7852bb8517d36a74ff2d
Simon Daigler <simon.daigler@student.kit.edu>
2025-09-10 18:15:41 +0200

    feat: added install combine script

--- utils/install_combine.sh
+++ utils/install_combine.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 ### Setup of CMSSW release
-CMSSW=CMSSW_12_6_5
+CMSSW=CMSSW_14_1_9
 
-export SCRAM_ARCH=slc7_amd64_gcc1000
+export SCRAM_ARCH=el9_amd64_gcc12
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
 
-scramv1 project $CMSSW
-pushd $CMSSW/src
+scramv1 project $CMSSW; pushd $CMSSW/src
 eval `scramv1 runtime -sh`
 
-# combine on 102X slc7
 git clone git@github.com:cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
-# cd HiggsAnalysis/CombinedLimit
-# git fetch origin
-# git checkout v8.0.1
-# cd -
+cd HiggsAnalysis/CombinedLimit
+git fetch origin
+git checkout v10.2.1
+cd -
 
-# CombineHarvester (current master for 102X)
+# CombineHarvester 
 git clone git@github.com:cms-analysis/CombineHarvester.git CombineHarvester
+cd CombineHarvester
+git fetch origin
+git checkout v3.0.0
 
 # SM analysis specific code
-git clone git@github.com:KIT-CMS/SMRun2Legacy.git CombineHarvester/SMRun2Legacy -b ul
-
-# TauID analysis specific code
-git clone git@github.com:conformist89/TauIDSFMeasurement.git CombineHarvester/TauIDSFMeasurement -b combined-fit
+git clone git@github.com:KIT-CMS/SMRun2Legacy.git SMRun2Legacy
+cd SMRun2Legacy
+git fetch origin
+git checkout ul
 
 # compile everything
-# Build
-scram b clean
-scram b -j 10
+CORES=`grep -c ^processor /proc/cpuinfo`
+scramv1 b clean; scramv1 b -j $CORES
